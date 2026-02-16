d39222186248e5eb1cf519f06ee2ca000807ec92
Ajitesh Kumar <ajinest6@gmail.com>
2026-02-16 18:53:27 -0500

    readme add info without docker-compose, backend.yml to build more

--- resetdb.sh
+++ resetdb.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # print the counts before resetting
-psql -U musicer -c "SELECT COUNT(*) FROM tracks;" -d music
-psql -U musicer -c "SELECT COUNT(*) FROM playlists;" -d music
+# psql -U musicer -c "SELECT COUNT(*) FROM tracks;" -d music
+# psql -U musicer -c "SELECT COUNT(*) FROM playlists;" -d music
 
 dropdb music --if-exists -f
 createdb music -O musicer
